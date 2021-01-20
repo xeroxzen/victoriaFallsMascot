5f11f5d7fbc2831c020c7374493ea5cf7f8ddadc
xeroxzen <andilembele020@gmail.com>
2021-01-20 13:56:28 +0200

    connected firebase credentials

--- index.js
+++ index.js
@@ -8,15 +8,13 @@ const dialogflow = require("dialogflow-fulfillment");
 //security credentials
 var admin = require("firebase-admin");
 
-var serviceAccount = require("path/to/serviceAccountKey.json");
+var serviceAccount = require("./config/victoriafallsmascot-imwo-firebase-adminsdk-2hs88-562506bac3.json");
 try {
-    admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount),
-    });
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
 
-    console.log("Connected to Database");
+  console.log("Connected to Database");
 } catch (err) {
-    console.log(`Error here ${err}`)
+  console.log(`Error here ${err}`);
 }
-
-
