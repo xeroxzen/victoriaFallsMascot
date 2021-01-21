95ff6b47cbb7e5e96dea47465fc7c7084e3191b5
xeroxzen <andilembele020@gmail.com>
2021-01-21 03:37:32 +0200

    Revert "let's test the db and see #2"
    This reverts commit f80f255b89b4b6bb7f39f629205ce19702e68ad1.

--- index.js
+++ index.js
@@ -103,11 +103,11 @@ app.post("/conversations", express.json(), (req, res) => {
     // data to be saved
     // age range, gender, symptoms, phone number, time
 
-    var ageRange = agent.context.get("covidGender").params.ageGroups;
-    var gender = agent.context.get("covidSymptoms").params.gender;
-    var symptoms = agent.context.get("coronavirusPhone-followup").params
+    var ageRange = agent.context.get("covidGender").parameters.ageGroups;
+    var gender = agent.context.get("covidSymptoms").parameters.gender;
+    var symptoms = agent.context.get("coronavirusPhone-followup").parameters
       .symptoms;
-    var phone = agent.context.get("capture-phone-number").params.phone;
+    var phone = agent.context.get("capture-phone-number").parameters.phone;
 
     // human readable date
     // const dateObject = new Date();
