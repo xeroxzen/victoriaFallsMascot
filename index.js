e5cbf6411dbc2c11351abb59409d1ef366274c65
Google Jr <andilembele020@gmail.com>
2021-01-22 03:09:21 +0200

    fix #2

--- index.js
+++ index.js
@@ -110,7 +110,7 @@ app.post("/conversations", express.json(), (req, res) => {
     //we need to save some data here
     // data to be saved
     // age range, gender, symptoms, phone number, time
-    // let parameters = request.body.queryResult.parameters;
+    let parameters = req.body.result.parameters;
 
     var ageRange = agent.context.get("covidGender").parameters["ageGroups"];
     var gender = agent.context.get("covidSymptoms").parameters["gender"];
