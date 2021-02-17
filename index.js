34524fc9c9d2daa04d2dc71da768f909f6f8cb99
Google Jr <andilembele020@gmail.com>
2021-02-17 12:44:25 +0200

    getPaymentsConfirmation() function #8

--- index.js
+++ index.js
@@ -210,13 +210,9 @@ app.post("/conversations", express.json(), (request, response) => {
   }
 
   function getPaymentsConfirmation(agent) {
-    const account = agent.context.get("paymentPhone").parameters.accountNumber
-      .original;
-    const phoneNumber = agent.context.get("paymentAmount").parameters[
-      "phone-number"
-    ].original;
-    const amount = agent.context.get("paymentConfirm").parameters.amount
-      .original;
+    const account = agent.parameters.accountNumber.original;
+    const phoneNumber = agent.parameters["phone-number"].original;
+    const amount = agent.parameters.amount.original;
 
     agent.add(
       `Account Number: ${account} \nPhone Number: ${phoneNumber} \nAmount: ${amount}`
