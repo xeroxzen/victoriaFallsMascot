4d5b366a7c8a2de40c6ead1968706dbdb37839d3
Google Jr <andilembele020@gmail.com>
2021-03-08 15:32:56 +0200

    still on keys #3

--- index.js
+++ index.js
@@ -291,7 +291,7 @@ app.post("/conversations", express.json(), (request, response) => {
     var paynow_id = process.env.INTEGRATION_ID;
     var paynow_key = process.env.INTEGRATION_KEY;
 
-    let paynow = new Paynow(`${paynow_id}, ${paynow_key}`);
+    let paynow = new Paynow("11700", "f9e7af51-2b09-4803-a115-44124734ec3e");
     let payment = paynow.createPayment(invoiceNumber, email);
     payment.add("Rates", parseFloat(amount.amount));
 
